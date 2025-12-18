b8e1911892b90bcb29d47222875be0a0ae367bd3
samiruggs <samiruggs2@gmail.com>
2025-12-18 09:07:35 +0100

    Update layoff cleaning.sql

--- layoff cleaning.sql
+++ layoff cleaning.sql
@@ -1,3 +1,31 @@
+USE layoffs
+GO
+
+SELECT *
+FROM
+dbo.layoff_staging;    -- Before this the UI interface was used to duplicate the layoff data and saving it as layoff_staging
+
+SELECT 
+SUM(CASE WHEN company IS NULL THEN 1 ELSE 0 END) AS company_nulls,
+SUM(CASE WHEN [location] IS NULL THEN 1 ELSE 0 END) AS location_nulls,
+SUM(CASE WHEN industry IS NULL THEN 1 ELSE 0 END) AS industry_nulls,
+SUM(CASE WHEN total_laid_off IS NULL THEN 1 ELSE 0 END) AS total_laid_off_nulls,
+SUM(CASE WHEN percentage_laid_off IS NULL THEN 1 ELSE 0 END) AS percentage_laid_off_nulls,
+SUM(CASE WHEN [date] IS NULL THEN 1 ELSE 0 END) AS date_nulls,
+SUM(CASE WHEN stage IS NULL THEN 1 ELSE 0 END) AS stage_nulls,
+SUM(CASE WHEN country IS NULL THEN 1 ELSE 0 END) AS country_nulls,
+SUM(CASE WHEN funds_raised_millions IS NULL THEN 1 ELSE 0 END) AS fund_raised_millions_nulls
+FROM dbo.layoff_staging;                                                                             -- Tried to find out the number of NULLS in each columns
+
+SELECT DISTINCT *
+INTO layoff_clean
+FROM layoff_staging;         -- copied layoff_staging into another back up data layoff_clean but for only distinct values.
+
+SELECT 
+    COUNT(*) - COUNT(total_laid_off) AS total_laid_off_nulls
+FROM dbo.layoff_staging;                                      -- An alternative way of finding the number of NULL in each column, here is just an example for one column
+
+
 USE [Layoffs]
 GO
 
@@ -10,18 +38,18 @@ SELECT [company]
       ,[stage]
       ,[country]
       ,[funds_raised_millions]
-  FROM [dbo].[layoff_clean]
+  FROM [dbo].[layoff_clean]             -- This is ascertain the the outcome of the new data created.        
 
-GO
+
 
 SELECT COUNT(*)
-FROM dbo.layoff_clean;
+FROM dbo.layoff_clean;      -- To ascertain the number of distinct rows
 
 SELECT COUNT(*) AS distinct_row_count
 FROM (
     SELECT DISTINCT *
     FROM dbo.layoff_staging
-) t;
+) t;                            -- An alternative way to acsertain the distinct row count.
 
 WITH cte AS(SELECT *,
 ROW_NUMBER() OVER(PARTITION BY 
@@ -107,3 +135,4 @@ FROM layoff_staging
 SELECT *
 FROM layoff_staging
 WHERE company = 'Carvana'
+
