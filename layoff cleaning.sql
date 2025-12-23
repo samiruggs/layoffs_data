a89e522d9d4ac0364a0b3f78bc7a07c7b2764ccf
samiruggs <samiruggs2@gmail.com>
2025-12-23 22:03:45 +0100

    Update layoff cleaning.sql

--- layoff cleaning.sql
+++ layoff cleaning.sql
@@ -1,9 +1,10 @@
+--- 1. Before this the UI interface was used to duplicate the layoff data and saving it as layoff_staging
 USE layoffs
 GO
 
 SELECT *
 FROM
-dbo.layoff_staging;    -- Before this the UI interface was used to duplicate the layoff data and saving it as layoff_staging
+dbo.layoff_staging;    
 
 SELECT 
 SUM(CASE WHEN company IS NULL THEN 1 ELSE 0 END) AS company_nulls,
@@ -17,15 +18,17 @@ SUM(CASE WHEN country IS NULL THEN 1 ELSE 0 END) AS country_nulls,
 SUM(CASE WHEN funds_raised_millions IS NULL THEN 1 ELSE 0 END) AS fund_raised_millions_nulls
 FROM dbo.layoff_staging;                                                                             -- Tried to find out the number of NULLS in each columns
 
+ --- 2. copied layoff_staging into another back up data layoff_clean but for only distinct values.
 SELECT DISTINCT *
 INTO layoff_clean
-FROM layoff_staging;         -- copied layoff_staging into another back up data layoff_clean but for only distinct values.
+FROM layoff_staging;        
 
+ --- 3. An alternative way of finding the number of NULL in each column, here is just an example for one column
 SELECT 
     COUNT(*) - COUNT(total_laid_off) AS total_laid_off_nulls
-FROM dbo.layoff_staging;                                      -- An alternative way of finding the number of NULL in each column, here is just an example for one column
+FROM dbo.layoff_staging;                                     
 
-
+--- 4. This is to ascertain the outcome of the new data created.   
 USE [Layoffs]
 GO
 
@@ -38,19 +41,22 @@ SELECT [company]
       ,[stage]
       ,[country]
       ,[funds_raised_millions]
-  FROM [dbo].[layoff_clean]             -- This is ascertain the the outcome of the new data created.        
+  FROM [dbo].[layoff_clean]                
 
-
+ --- 5. To ascertain the number of distinct rows
 
 SELECT COUNT(*)
-FROM dbo.layoff_clean;      -- To ascertain the number of distinct rows
+FROM dbo.layoff_clean;     
+
+ --- 6. An alternative way to ascertain the distinct row count.
 
 SELECT COUNT(*) AS distinct_row_count
 FROM (
     SELECT DISTINCT *
     FROM dbo.layoff_staging
-) t;                            -- An alternative way to acsertain the distinct row count.
+) t;                           
 
+--- 7. This creates a new column rn to determine duplicated rows/ an alternative way to find duplicates. The duplicates are deleted by changing (SELECT * FROM cte) to DELETE FROM cte
 
 WITH cte AS(SELECT *,
 ROW_NUMBER() OVER(PARTITION BY 
@@ -66,67 +72,85 @@ industry
       ORDER BY country) AS rn
   FROM [Layoffs].[dbo].[layoff_staging])
   SELECT * FROM cte
-  WHERE rn > 1                            -- this creates a new column rn to determine duplicated rows/ an alternative way to find duplicates
-                                          -- the duplicates are deleted by chaning (SELECT * FROM cte) to DELETE FROM cte
+  WHERE rn > 1                            
 
+ --- 8. Select distinct country in alphabetical order for standardization
 
 SELECT DISTINCT country
 FROM layoff_staging
-ORDER BY 1                     -- select distinct country in alphavetical order for standardization
+ORDER BY 1                    
+
+ --- 9. Standardized United States. This and above was donefor text rows like industry, location etc
 
 UPDATE layoff_staging
 SET country = TRIM(REPLACE(country,'.',''))
-WHERE country LIKE 'United States%'         -- Standardized United States. This and above was donefor text rows like industry, location etc
+WHERE country LIKE 'United States%'        
+
+--- 10. Identify null values in the country colunn, same is done with other text columns.   
+SELECT *
+FROM layoff_staging
+WHERE country IS NULL OR country =''       
+
+--- 11. Airbnb row has an empty value so is seperated out for better understanding.
 
 SELECT *
 FROM layoff_staging
-WHERE country IS NULL OR country = ''       -- Identify null values in the country colunn, same is done with other text columns.
+WHERE company = 'Airbnb'  
 
+--- 12. The empty cell is replaced with Travel, as is obtainable with the others.   
+    
+UPDATE layoff_staging
+SET industry = 'Travel'
+WHERE company = 'Airbnb'           
+
+--- 13. Same as done in 12
+    
 SELECT *
 FROM layoff_staging
-WHERE company = 'Airbnb'            -- Airbnb row has an empty value so is seperated out for better understanding.
+WHERE company = 'Juul' 
+
+--- 14. Same as done in 12
 
 UPDATE layoff_staging
 SET industry = 'Travel'
-WHERE company = 'Airbnb'           --the empty cell is replaced eith Travel as is obtainable with the others.
+WHERE company = 'Airbnb'        
+
+--- 15. Same as done in 12
 
 SELECT *
 FROM layoff_staging
-WHERE company = 'Juul'             -- same is done here
+WHERE company = 'Carvana'           
+
+--- 16. Same as done in 12
 
 UPDATE layoff_staging
 SET industry = 'Travel'
-WHERE company = 'Airbnb'          -- same is done here
+WHERE company = 'Airbnb'     
 
-SELECT *
-FROM layoff_staging
-WHERE company = 'Carvana'            --  here too
+--- 17. Identifying null values in the number fields
 
-UPDATE layoff_staging
-SET industry = 'Travel'
-WHERE company = 'Airbnb'             -- here too
-           
 SELECT total_laid_off, percentage_laid_off
 FROM layoff_staging
-WHERE total_laid_off IS NULL OR percentage_laid_off IS NULL   --identifying null value in the number fields
+WHERE total_laid_off IS NULL OR percentage_laid_off IS NULL   
+
+--- 17. Identifying null values in the industry fields
 
 SELECT *
 FROM layoff_staging
-WHERE industry IS NULL OR industry = ''    -- same
+WHERE industry IS NULL OR industry = ''    
 
-SELECT total_laid_off, percentage_laid_off
-FROM layoff_staging
-WHERE total_laid_off IS NULL OR percentage_laid_off IS NULL;
-
+--- 18. Delete null values as the information is incomplete
 
 WITH cte AS (SELECT *
     FROM layoff_staging
     WHERE total_laid_off IS NULL OR percentage_laid_off IS NULL) 
 DELETE FROM cte
-WHERE percentage_laid_off IS NULL OR total_laid_off IS NULL;   ]-- delete or null value as information is incomplete
+WHERE percentage_laid_off IS NULL OR total_laid_off IS NULL;  
 
+--- 19. load the final and cleaned outcome
 SELECT *
 FROM layoff_staging
+
 
 
 
